d690df23efa6d9e9fda2e551ff13933973d2e91a
Naufal <naufalchumaidi001@gmail.com>
2026-03-05 09:34:43 +0800

    tambah file about

---
+++ laravelTIB/resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <p>Ini adalah website pertama saya menggunakan Laravel.</p>
+</body>
+</html>
